713f10a438863ecb9d9f7ed5514921a61d6eda42
加藤公一 <kato_ko@monosense.co.jp>
2016-01-13 16:24:24 +0900

    javascript演習1

---
+++ jsExercise.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>JavaScript演習1</title>
+  <script>
+    function sayHello() {
+      alert('Hello World');
+    }
+  </script>
+</head>
+<body>
+  <button type="button" name="button" onclick="sayHello();">Say Hello</button>
+</body>
+</html>
