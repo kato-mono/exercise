bbdc2253385c5902019ca45095a62ba4194e5124
加藤公一 <kato_ko@monosense.co.jp>
2016-01-19 16:17:58 +0900

    modelをDBクラスのラップ関数を使うように変更

--- fuel/fuel/app/classes/model/todo.php
+++ fuel/fuel/app/classes/model/todo.php
@@ -4,7 +4,8 @@ class Model_Todo extends \Model {
 
   public static function select_all()
   {
-    $query = DB::query('SELECT * FROM ensyu.todo');
+    $query = DB::select('*');
+    $query->from('ensyu.todo');
     return $query->execute();
   }
 
@@ -12,32 +13,38 @@ class Model_Todo extends \Model {
   {
     $status = 0; // ステータスの初期状態
 
-    $query = DB::query('INSERT INTO ensyu.todo (status, description) VALUES(:status, :description);');
-    $query->param('status', $status);
-    $query->param('description', $description);
+    $query = DB::insert('ensyu.todo');
+    $query->set(array(
+      'status' => $status,
+      'description' => $description
+    ));
     $query->execute();
   }
 
   public static function update_status($id, $status)
   {
-    $query = DB::query('UPDATE ensyu.todo SET status=:status WHERE id=:id');
-    $query->param('id', $id);
-    $query->param('status', $status);
+    $query = DB::update('ensyu.todo');
+    $query->set(array(
+      'status' => $status
+    ));
+    $query->where('id', $id);
     $query->execute();
   }
 
   public static function update_disctiption($id, $description)
   {
-    $query = DB::query('UPDATE ensyu.todo SET description=:description WHERE id=:id');
-    $query->param('id', $id);
-    $query->param('description', $description);
+    $query = DB::update('ensyu.todo');
+    $query->set(array(
+      'description' => $description
+    ));
+    $query->where('id', $id);
     $query->execute();
   }
 
   public static function delete_task($id)
   {
-    $query = DB::query('DELETE FROM ensyu.todo WHERE id=:id');
-    $query->param('id', $id);
+    $query = DB::delete('ensyu.todo');
+    $query->where('id', $id);
     $query->execute();
   }
 }
