8abfd6a4c3f4445e6d812b6f999712eb2b7452d0
加藤公一 <kato_ko@monosense.co.jp>
2016-02-15 19:26:25 +0900

    twitter apiとの窓口クラス

--- tweet_storage/app/TwitterMediator.php
+++ tweet_storage/app/TwitterMediator.php
@@ -1,10 +1,12 @@
 <?php
 
-require_once './path.php';
+namespace Monosense\Exercise;
+
+require_once './Path.php';
 require_once Path::VENDOR_AUTOLOAD;
 require_once Path::APP_FILE_UTIL;
 
-class Teller
+class TwitterMediator
 {
     // twitter connection settings
     private $api_key;
@@ -14,38 +16,51 @@ class Teller
     public function __construct()
     {
         $twitter_configs_file_path = Path::CONFIG_TWITTER_CONNECTION;
-        $twitter_configs = File_Util::load_json_config($twitter_configs_file_path);
+        $twitter_configs = FileUtil::loadJsonConfig($twitter_configs_file_path);
 
         // (api_key, api_secret)、別名(consumer_key, consumer_secret)
         $this->api_key = $twitter_configs['consumer_key'];
         $this->api_secret = $twitter_configs['consumer_secret'];
 
-        $this->fetch_twitter_bearer_token();
+        $this->fetchTwitterBearerToken();
     }
 
-    public function fetch_10_tweets()
+    /**
+     * 最新10件のツイートを取得する.
+     *
+     * @return string JSON
+     */
+    public function fetch10Tweets()
     {
         $request_url = Path::URL_TWITTER_FETCH_TIMELINE;
 
         $fetch_condition = [
             'screen_name' => '@ka_ko_mo_no',
             'count' => 10,
+            'trim_user' => true,
         ];
 
         if ($fetch_condition) {
             $request_url .= '?'.http_build_query($fetch_condition);
         }
 
-        $context = $this->make_bearer_context_get();
+        $context = $this->makeBearerContextGet();
 
-        return file_get_contents(
+        $response = file_get_contents(
             $request_url,
             false,
             $context
         );
+
+        return $this->formatTwitterResponse($response);
     }
 
-    private function fetch_twitter_bearer_token()
+    /**
+     * ベアラートークンを取得する.
+     *
+     * @return string
+     */
+    private function fetchTwitterBearerToken()
     {
         $credential = base64_encode($this->api_key.':'.$this->api_secret);
         $context = stream_context_create([
@@ -69,7 +84,33 @@ class Teller
         $this->bearer_token = json_decode($response_json)->access_token;
     }
 
-    private function make_bearer_context_get()
+    /**
+     * twitterAPIから返されるレスポンスを逆順、かつ、idをキーに配置した状態に整形する.
+     * (idをキーに配置するのは後々マージなどしやすくするため).
+     *
+     * @param string $response JSON
+     *
+     * @return string JSON
+     */
+    private function formatTwitterResponse($response)
+    {
+        // 最新のツイートが頭となるので逆順にする
+        $reverse_tweets = array_reverse(json_decode($response, true));
+
+        $formated_reverse_tweets = [];
+        foreach ($reverse_tweets as $tweet) {
+            $formated_reverse_tweets[$tweet['id']] = $tweet;
+        }
+
+        return json_encode($formated_reverse_tweets);
+    }
+
+    /**
+     * ベアラートークンによるGET通信用のcontextを生成する.
+     *
+     * @return array
+     */
+    private function makeBearerContextGet()
     {
         return stream_context_create([
             'http' => [
